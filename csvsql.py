705204bf46b7347456fe2a289343179fb448ced5
rs86 <rs86@users.noreply.github.com>
2015-07-05 19:33:23 -0300

    Create csvsql.py

---
+++ csvsql.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+import sys
+
+conn = sqlite3.connect(':memory:')
+
+df = pd.read_csv(sys.stdin)
+df.columns = df.columns.map(lambda x: x.replace(" ", ""))
+df.to_sql('t', conn)
+
+df = pd.read_sql(sys.argv[1], conn)
+
+print df
